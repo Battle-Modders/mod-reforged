071f48e8b3875497989a3cf6eb91ad4525dbd40f
taroeld <tarob@live.de>
2023-01-26 09:27:28 +0100

    feat: add further check if rawHTML has content

--- ui/mods/mod_reforged_setup.js
+++ ui/mods/mod_reforged_setup.js
@@ -7,7 +7,7 @@ var addHeaderDescriptionDiv = TooltipModule.prototype.addHeaderDescriptionDiv;
 TooltipModule.prototype.addHeaderDescriptionDiv = function(_parentDIV, _data)
 {
 	var ret = addHeaderDescriptionDiv.call(this, _parentDIV, _data);
-	if (ret === null || _data.rawHTML === undefined)
+	if (ret === null || _data.rawHTML === undefined || _data.rawHTML === "")
 		return ret;
 	var description = ret.find(".description:first");
 	description.html(description.html() + _data.rawHTML);
