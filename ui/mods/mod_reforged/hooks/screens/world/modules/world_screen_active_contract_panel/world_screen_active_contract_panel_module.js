4d67de8a949319df7858fb88ad748b5579029c99
taroeld <tarob@live.de>
2023-01-28 21:30:16 +0100

    feat: add commented out tooltip setter

--- ui/mods/mod_reforged/hooks/screens/world/modules/world_screen_active_contract_panel/world_screen_active_contract_panel_module.js
+++ ui/mods/mod_reforged/hooks/screens/world/modules/world_screen_active_contract_panel/world_screen_active_contract_panel_module.js
@@ -7,3 +7,18 @@ WorldScreenActiveContractPanelModule.prototype.createDIV = function(_parentDiv)
 		SQ.call(this.mSQHandle, 'onActiveContractDetailsClicked');
 	}, this));
 }
+
+// Insert after tooltips were added
+// Reforged.Hooks.WorldScreenActiveContractPanelModule_bindTooltips = WorldScreenActiveContractPanelModule.prototype.bindTooltips;
+// WorldScreenActiveContractPanelModule.prototype.bindTooltips = function ()
+// {
+// 	Reforged.Hooks.WorldScreenActiveContractPanelModule_bindTooltips.call(this);
+//     this.mContentContainer.bindTooltip({ contentType: 'msu-generic', modId: Reforged.ID, elementId: "Reforged.Contract.ContentContainer"});
+// };
+
+// Reforged.Hooks.WorldScreenActiveContractPanelModule_unbindTooltips = WorldScreenActiveContractPanelModule.prototype.unbindTooltips;
+// WorldScreenActiveContractPanelModule.prototype.unbindTooltips = function ()
+// {
+// 	Reforged.Hooks.WorldScreenActiveContractPanelModule_unbindTooltips.call(this);
+// 	this.mContentContainer.unbindTooltip();
+// };
