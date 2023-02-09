7418c5022b9b0464d5edbc04823d3138cb6ba52e
LordMidas <55047920+LordMidas@users.noreply.github.com>
2023-02-09 13:34:20 -0500

    feat: add reforged main menu logo and set main menu to use this logo
    Art by Von Krolock.

--- ui/mods/mod_reforged/reforged_assets.js
+++ ui/mods/mod_reforged/reforged_assets.js
@@ -2,5 +2,6 @@
 "use strict";
 
 Reforged.Asset = {
-	BUTTON_RETREAT_WIN : 'ui/skin/icon_rf_retreat_win.png'
+	BUTTON_RETREAT_WIN : 'ui/skin/icon_rf_retreat_win.png',
+	MAIN_MENU_LOGO: 'ui/skin/rf_main_menu_logo.png'
 };
