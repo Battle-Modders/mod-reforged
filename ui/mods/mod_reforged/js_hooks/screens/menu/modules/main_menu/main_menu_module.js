dbc19612d6a497bcfab387599fc681d4168128bf
LordMidas <55047920+LordMidas@users.noreply.github.com>
2023-03-02 09:40:37 -0500

    refactor: add hook to set the logo on pause menu to use reforged logo

--- ui/mods/mod_reforged/js_hooks/screens/menu/modules/main_menu/main_menu_module.js
+++ ui/mods/mod_reforged/js_hooks/screens/menu/modules/main_menu/main_menu_module.js
@@ -1,1 +1,8 @@
 Screens.MainMenuScreen.getModule("MainMenuModule").mContainer.find("img").attr('src', Path.GFX + Reforged.Asset.MAIN_MENU_LOGO).css('top', "-7.8rem").css('left', "-6rem");
+
+Reforged.Hooks.MainMenuModule_createDIV = MainMenuModule.prototype.createDIV;
+MainMenuModule.prototype.createDIV = function (_parentDiv)
+{
+	Reforged.Hooks.MainMenuModule_createDIV.call(this, _parentDiv);
+	this.mContainer.find("img").attr('src', Path.GFX + Reforged.Asset.MAIN_MENU_LOGO).css('top', "-3.8rem").css('left', "-6rem");
+}
