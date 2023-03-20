f9c4fe31caa8f4d995a4547c6aef8a0f63e78e05
Darxo <Darxo@KAISERTOM>
2023-03-20 11:49:58 +0100

    feat: implement new Wait-All Button

--- ui/mods/mod_reforged/reforged_assets.js
+++ ui/mods/mod_reforged/reforged_assets.js
@@ -2,6 +2,7 @@
 "use strict";
 
 Reforged.Asset = {
+	BUTTON_WAIT_ALL_TURNS: 'ui/skin/rf_icon_wait_all.png',
 	BUTTON_RETREAT_WIN : 'ui/skin/icon_rf_retreat_win.png',
 	MAIN_MENU_LOGO: 'ui/skin/rf_main_menu_logo.png'
 };
