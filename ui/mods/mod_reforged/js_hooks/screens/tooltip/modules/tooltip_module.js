ae7afecef38015ac4586916e69eff8c1f0ff3f3a
Darxo <Darxo@KAISERTOM>
2023-07-30 10:14:17 +0200

    fix: some UITooltip going out of bounds, e.g. famed items in markets

--- ui/mods/mod_reforged/js_hooks/screens/tooltip/modules/tooltip_module.js
+++ ui/mods/mod_reforged/js_hooks/screens/tooltip/modules/tooltip_module.js
@@ -19,3 +19,45 @@ TooltipModule.prototype.setupTileTooltip = function()
 	// apply the calculated topPos
 	this.mContainer.css({ top: posTop });
 }
+
+Reforged.Hooks.TooltipModule_setupUITooltip = TooltipModule.prototype.setupUITooltip;
+TooltipModule.prototype.setupUITooltip = function(_targetDIV, _data)
+{
+	Reforged.Hooks.TooltipModule_setupUITooltip.call(this, _targetDIV, _data);
+
+	// We do the mostly the same calculations as vanilla at first for getting the top position of our tooltip
+	if(_targetDIV === undefined) return;
+	var offsetY = ('yOffset' in _data) ? _data.yOffset : this.mDefaultYOffset;
+	if (offsetY !== null)
+	{
+		if (typeof(offsetY) === 'string')
+		{
+			offsetY = parseInt(offsetY, 10);
+		}
+		else if (typeof(offsetY) !== 'number')
+		{
+			offsetY = 0;
+		}
+	}
+
+	var targetOffset    = _targetDIV.offset();
+	var elementHeight   = _targetDIV.outerHeight(true);
+	var containerHeight = this.mContainer.outerHeight(true);
+	// By default we want the tooltips shown on top of the UI-Element
+	var posTop  = targetOffset.top - containerHeight - offsetY;
+
+	// If that would overflow the top of the screen, we instead display them below our cursor
+	if (posTop < 0)
+	{
+		posTop = targetOffset.top + elementHeight + offsetY;
+	}
+
+	// If that would overflow the bottom of the screen, we instead display it starting directly at the top of the screen, consequentially overlapping with our UI-Element and cursor
+	var wnd = this.mParent; // $(window);
+	if ((posTop + containerHeight + offsetY) > wnd.height())
+	{
+		posTop = 10;
+	}
+
+	this.mContainer.css({ top: posTop });
+}
