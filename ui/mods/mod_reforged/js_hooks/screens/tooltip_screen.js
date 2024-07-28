e98cf40ca6f32c4f2d53d0ba8f834d8946eb289a
taroeld <tarob@live.de>
2024-07-28 11:15:10 +0200

    feat: improve look of attribute tooltip and add reach tooltip

--- ui/mods/mod_reforged/js_hooks/screens/tooltip_screen.js
+++ ui/mods/mod_reforged/js_hooks/screens/tooltip_screen.js
@@ -1,10 +1,28 @@
+TooltipModule.prototype.rf_unescapeTagsOnElement = function(_elem)
+{
+	// TODO: integrate into MSU (need to do this for other tooltip elements too)
+	var text = _elem.html();
+	text = text.replace(/&lt;/g, "<"); // unescape HTML tag brackets
+	text = text.replace(/&gt;/g, ">"); // unescape HTML tag brackets
+	_elem.html(text);
+}
+
 Reforged.Hooks.TooltipModule_addHeaderDescriptionDiv = TooltipModule.prototype.addHeaderDescriptionDiv;
 TooltipModule.prototype.addHeaderDescriptionDiv = function(_parentDIV, _data)
 {
 	var ret = Reforged.Hooks.TooltipModule_addHeaderDescriptionDiv.call(this, _parentDIV, _data);
-	if (ret === null || _data.rawHTML === undefined || _data.rawHTML === "")
+	if (ret === null || _data.rawHTMLInText !== true )
 		return ret;
-	var description = ret.find(".description:first");
-	description.html(description.html() + _data.rawHTML);
+	this.rf_unescapeTagsOnElement(ret.find(".description:first"));
+	return ret;
+};
+
+Reforged.Hooks.TooltipModule_addContentTextDiv = TooltipModule.prototype.addContentTextDiv;
+TooltipModule.prototype.addContentTextDiv = function(_parentDIV, _data, _isChildRow, _isParentFullWidth)
+{
+	var ret = Reforged.Hooks.TooltipModule_addContentTextDiv.call(this, _parentDIV, _data, _isChildRow, _isParentFullWidth);
+	if (ret === null || _data.rawHTMLInText !== true )
+		return ret;
+	this.rf_unescapeTagsOnElement(ret.find(".text:first"));
 	return ret;
 };
