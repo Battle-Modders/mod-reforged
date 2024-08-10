c6780b7db87b3bd9913a09f192918d4cad6242ae
taroeld <tarob@live.de>
2024-08-10 23:12:47 +0200

    feat: replace Morale with Reach in the character screen progress bar

--- ui/mods/mod_reforged/reforged_assets.js
+++ ui/mods/mod_reforged/reforged_assets.js
@@ -6,3 +6,11 @@ Reforged.Asset = {
 	BUTTON_RETREAT_WIN : 'ui/skin/icon_rf_retreat_win.png',
 	MAIN_MENU_LOGO: 'ui/skin/rf_main_menu_logo.png'
 };
+
+ProgressbarStyleIdentifier["rf_Reach"] = 'rf_reach';
+ProgressbarValueIdentifier["rf_Reach"] = 'rf_reach';
+ProgressbarValueIdentifier["rf_ReachMax"] = 'rf_reachMax';
+ProgressbarValueIdentifier["rf_ReachLabel"] = 'rf_reachLabel';
+Asset["rf_Reach"] = "ui/icons/reach.png";
+Asset["rf_ReachAttack"] = "ui/icons/reach_attack.png";
+Asset["rf_ReachDefense"] = "ui/icons/reach_defense.png";
