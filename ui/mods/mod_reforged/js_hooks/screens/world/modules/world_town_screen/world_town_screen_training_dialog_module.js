b6fe3c6e1a8f49db606fdde193c0d34bb3710315
taroeld <tarob@live.de>
2025-03-01 23:50:35 +0100

    fix: tooltips overwriting each other

--- ui/mods/mod_reforged/js_hooks/screens/world/modules/world_town_screen/world_town_screen_training_dialog_module.js
+++ ui/mods/mod_reforged/js_hooks/screens/world/modules/world_town_screen/world_town_screen_training_dialog_module.js
@@ -1,18 +1,15 @@
 Reforged.Hooks.WorldTownScreenTrainingDialogModule_createTrainingControlDIV= WorldTownScreenTrainingDialogModule.prototype.createTrainingControlDIV;
 WorldTownScreenTrainingDialogModule.prototype.createTrainingControlDIV = function(_i, _parentDiv, _entityID, _data, _money)
 {
-	console.error("createTrainingControlDIV")
-	Reforged.Hooks.WorldTownScreenTrainingDialogModule_createTrainingControlDIV.call(_i, _parentDiv, _entityID, _data, _money);
+	Reforged.Hooks.WorldTownScreenTrainingDialogModule_createTrainingControlDIV.call(this, _i, _parentDiv, _entityID, _data, _money);
 	if (_data.id in [0, 1, 2])
 		return;
 	var tooltipID = "PerkGroup+" + _data.id;
 	var tooltipData = { contentType: 'msu-generic', modId: DynamicPerks.ID, elementId: tooltipID };
 
-	var icon = _parentDiv.find('img.is-icon');
-	console.error("icon len " + icon.length)
+	var icon = _parentDiv.find('img.is-icon').last();
 	icon.bindTooltip(tooltipData);
 
-	var name = _parentDiv.find('div.is-name');
-	console.error("name len " + name.length)
+	var name = _parentDiv.find('div.is-name').last();
 	name.bindTooltip(tooltipData);
 }
